b7ae369c7740eab677af4c0323d8c209873ada58
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-10-10 06:02:52 -0300

    add component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   {
     path:'login', component:LoginComponent
-  }
+  },
+  /*agregar ruta para dashboard */
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './dashboard/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
